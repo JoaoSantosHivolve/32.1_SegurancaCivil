8dfa02504fb6653adcc08da1562bbe323c5402be
JoaoLucas <joao.santos@hivolve.com>
2020-12-03 15:48:40 +0000

    New Version
    Movement and doors working

---
+++ Assets/Scripts/Environment/DoorOpeningLimiter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpeningLimiter : MonoBehaviour
+{
+    public Vector3 rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rot = transform.eulerAngles;
+        var rotY = rot.y;
+        if (rotY <= -80) rotY = -80;
+        if (rotY >= 80) rotY = 80;
+
+        transform.localEulerAngles = new Vector3(rot.x, rotY, rot.z);
+        transform.localPosition = new Vector3(0.4f, 0, 0);
+    }
+}
---
+++ Assets/Scripts/VR/PlayerHeightBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHeightBehaviour : MonoBehaviour
+{
+    private CharacterController m_Collider;
+    public Transform cameraPosition;
+
+    public bool debugMode;
+    public GameObject debugCollider;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Collider = GetComponent<CharacterController>();
+        debugCollider.SetActive(debugMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Collider.height = cameraPosition.localPosition.y;
+        m_Collider.center = new Vector3(cameraPosition.localPosition.x, m_Collider.height / 2f, cameraPosition.localPosition.z);
+
+        if (debugMode)
+        {
+            var position = cameraPosition.localPosition;
+            debugCollider.transform.localPosition = new Vector3(position.x, position.y / 2, position.z);
+            var scale = debugCollider.transform.localScale;
+            debugCollider.transform.localScale = new Vector3(scale.x, m_Collider.height / 2, scale.z);
+        }
+    }
+}
