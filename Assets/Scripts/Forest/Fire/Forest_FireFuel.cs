120a1945a08dbc35158ba7fa68e272b5d7f44f86
JoaoLucas <joao.santos@hivolve.com>
2021-03-08 19:40:45 +0000

    Fire fuel Working

---
+++ Assets/Scripts/Forest/Fire/Forest_FireFuel.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85f54bd87ebfe5d40149847e9b8ada691fc1b22ad30aed213c4fd36484b48004
+size 496
--- Assets/Scripts/Forest/Fire/Forest_FireSource.cs
+++ Assets/Scripts/Forest/Fire/Forest_FireSource.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e948df96c72218275e43720a7b1f0e9afabe9b70761711c8d60c08e5a37483e
-size 977
+oid sha256:e38a52685f045087fe552f832ec8f0ac04fd4423ef147da6814e5f23de825a59
+size 1780
